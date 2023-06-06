65caa02b8a2d8fbd8816c93ea9a3187b7639e48f
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-06-06 18:08:06 -0300

    feat: Requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -24,5 +24,25 @@ def exists_word(word, instance: Queue):
     return informations
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word, instance: Queue):
+    informations = []
+    for index in range(len(instance)):
+        data = instance.search(index)
+        lines = []
+        for position, line in enumerate(data["linhas_do_arquivo"]):
+            if word.casefold() in line.casefold():
+                lines.append(
+                    {
+                        "linha": position + 1,
+                        "conteudo": line,
+                    }
+                )
+        if len(lines) > 0:
+            informations.append(
+                {
+                    "palavra": word,
+                    "arquivo": data["nome_do_arquivo"],
+                    "ocorrencias": lines,
+                }
+            )
+    return informations
