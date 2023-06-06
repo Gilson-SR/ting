a9301672e8dff5c64ad9a3e4680c6d5adb2110b1
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-06-06 16:24:48 -0300

    feat: Requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,7 +5,7 @@ import sys
 def process(path_file, instance):
     for index in range(len(instance)):
         if instance.search(index)["nome_do_arquivo"] == path_file:
-            return print(f"Arquivo {path_file} já foi processado.")
+            return print(f"Arquivo {path_file} processado com sucesso.")
     lines = txt_importer(path_file)
     data = dict(
             nome_do_arquivo=path_file,
@@ -17,7 +17,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        sys.stdout.write("Não há elementos\n")
+        return
+    data_removed = instance.dequeue()
+    name = data_removed["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {name} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
