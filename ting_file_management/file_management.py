7e596d6f13760fbc645ec326573a8aa90ad2c56a
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-06-06 15:16:19 -0300

    feat: Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação!"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido")
+
+    data = None
+    try:
+        with open(path_file, mode="r") as file:
+            data = file.read().split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    return data
