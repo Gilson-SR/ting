1f88d9359fba7074ca066b6053c0285eb605be8c
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-06-06 17:29:13 -0300

    feat: Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    informations = []
+    for index in range(len(instance)):
+        data = instance.search(index)
+        lines = []
+        for position, line in enumerate(data["linhas_do_arquivo"]):
+            if word.casefold() in line.casefold():
+                lines.append(
+                    {
+                        "linha": position + 1,
+                    }
+                )
+        if len(lines) > 0:
+            informations.append(
+                {
+                    "palavra": word,
+                    "arquivo": data["nome_do_arquivo"],
+                    "ocorrencias": lines,
+                }
+            )
+    return informations
 
 
 def search_by_word(word, instance):
