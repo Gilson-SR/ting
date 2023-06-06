00d530d9f606ec9eb0d852eaf18ff21373db8317
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-06-06 15:54:17 -0300

    feat: Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return print(f"Arquivo {path_file} já foi processado.")
+    lines = txt_importer(path_file)
+    data = dict(
+            nome_do_arquivo=path_file,
+            qtd_linhas=len(lines),
+            linhas_do_arquivo=lines
+        )
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
 
 
 def remove(instance):
